4afb9966248a74d58a84a5ae64b7a6e7e0bbe96b
reinaldocoronel <reinaldocoronel1708@gmail.com>
2020-06-18 13:42:03 -0500

    models first try

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Topings(models.Model):
+	name = models.CharField(max_length=64)
+	#available = models.boolean
+	#for_ = onetomanyrelationship 
+
+class Items(models.Model):
+	name = models.CharField(max_length=15, unique=True)
+	tipe = models.CharField(max_length=10)
+	extras = models.IntegerField(null=True)
+
+class Prices(models.Model):
+	price = models.FloatField()
+	tipe  = models.CharField(max_length=4)
+	item  = models.ForeignKey(Items, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f"{self.price:.2f}$"
+
+	class meta:
+		unique_together = ('item', 'tipe',)
--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
+from .models import Prices, Items
 # Create your tests here.
+class ModelsTestCase(TestCase):
+
+	def setUp(self):
+		m1 = Items.objects.create(name="2 topings", tipe="regular", extras=2)
+		m2 = Items.objects.create(name="ceasar", tipe="salad")
+		m1.prices_set.create(price=21.95, tipe='larg')
+		m1.prices_set.create(price=15.20, tipe='smal')
+		m2.prices_set.create(price=8.25, tipe='smal')
+	
+	def test_items_count(self):
+		menu = Items.objects.all()
+		self.assertEqual(menu.count(), 2)
+
+	def test_items_prices_large(self):
+		item = Items.objects.get(name="2 topings")
+		price_larg = item.prices_set.get(tipe='larg')
+		self.assertEqual(str(price_larg), "21.95$")
+
+	def test_items_price(self):
+		item = Items.objects.get(name="ceasar")
+		price = item.prices_set.first()
+		self.assertEqual(str(price), "8.25$")
